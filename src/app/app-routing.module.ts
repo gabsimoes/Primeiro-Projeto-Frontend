6fe99574d7dbdb2ca32700f7ffe98c4a33c0ccd9
Gabrielle Simões <gabrielle.simoes01@etec.sp.gov.br>
2021-10-22 22:04:24 -0300

    Primeiro Projeto Frontend no ANGULAR

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { CadastrarProdutosComponent } from './components/produtos/cadastrar-produtos/cadastrar-produtos.component';
+import { ListarProdutosComponent } from './components/produtos/listar-produtos/listar-produtos.component';
+import { HomeComponent } from './components/home/home.component';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'produtos', component:ListarProdutosComponent},
+  {path:'produtos/cadastrar', component:CadastrarProdutosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
